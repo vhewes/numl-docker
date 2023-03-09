44ddc2308a0db3eb0292bfc8343cfda93d7c26e3
v hewes <vhewes@fnal.gov>
2023-03-08 19:43:00 -0500

    remove apptainer build script

--- build-apptainer.sh
+++
@@ -1,5 +0,0 @@
-if [ $(hostname) == "Heimdall" ]; then
-  export APPTAINER_CACHEDIR=/raid/apptainer/.cache
-fi  
-apptainer build --fix-perms numl:torch1.13-cu11.7.sif docker://vhewes/numl:torch1.12-cu11.6
-apptainer build --fix-perms numl:torch1.13-cu11.7.sif docker://vhewes/numl:torch1.13-cu11.7
